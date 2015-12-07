f2ba79200ffb837107975ff157a1c09c2b89af02
Icenowy Zheng <icenowy@outlook.com>
2015-12-08 00:30:07 +0800

    gen_flags: gen flags.sh with "export"

--- utils/gen_flags/output.sh
+++ utils/gen_flags/output.sh
@@ -5,7 +5,7 @@ rm -f $OUTPUT_FILE
 out() {
 	while [ "$1" ]; do
 		if [ "x$(eval echo \$$1)" != "x" ]; then
-			echo "$1=\"$(eval echo \$$1)\"" >> $OUTPUT_FILE
+			echo "export $1=\"$(eval echo \$$1)\"" >> $OUTPUT_FILE
 		fi
 		shift
 	done
