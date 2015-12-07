6e6fd435c2f3c8f78fd1bd62c8866a1d055c071f
Icenowy Zheng <icenowy@outlook.com>
2015-12-08 01:32:31 +0800

    binutils: build and can be cached

--- toolchain/binutils/pass1.sh
+++ toolchain/binutils/pass1.sh
@@ -1,7 +1,11 @@
-mkdir -p out/build/binutils-pass1
-
 source toolchain/binutils/ver.sh
 
-utils/downloader/downloader toolchain/binutils/down/$binutils_ver || exit 1
+fprint="$(mktemp)"
 
+echo $PWD/out/tools $AOSC_EC_ARCH $AOSC_EC_LIBC $AOSC_EC_TRIPLET > $fprint
 
+utils/build_cache/cache_build binutils-pass1-$binutils_ver-$AOSC_EC_TRIPLET $fprint $PWD/out/build/binutils-pass1/destdir toolchain/binutils/pass1_build.sh
+
+rm $fprint
+
+cp -r $PWD/out/build/binutils-pass1/destdir/$PWD/out/tools/* $PWD/out/tools/
---
+++ toolchain/binutils/pass1_build.sh
@@ -0,0 +1,21 @@
+source toolchain/binutils/ver.sh
+
+utils/downloader/downloader toolchain/binutils/down/$binutils_ver || exit 1
+
+tools_prefix=$PWD/out/tools
+downloads_prefix=$PWD/out/download
+
+
+rm -rf out/build/binutils-pass1 &&
+mkdir -p out/build/binutils-pass1 &&
+cd out/build/binutils-pass1 &&
+
+tar xvf $downloads_prefix/binutils-${binutils_ver}.tar.bz2 &&
+
+mkdir build &&
+cd build &&
+../binutils-${binutils_ver}/configure --prefix=$tools_prefix --target=$AOSC_EC_TRIPLET \
+	--with-sysroot=$tools_prefix/$AOSC_EC_TRIPLET --disable-nls --disable-multilib &&
+make configure-host &&
+make &&
+make DESTDIR=$PKGDIR install
