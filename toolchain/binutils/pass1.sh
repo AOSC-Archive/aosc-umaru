795f4185a98f6ca910495f4e90d8f536272df218
Icenowy Zheng <icenowy@outlook.com>
2015-12-08 10:25:37 +0800

    binutils: fix build system

--- toolchain/binutils/pass1.sh
+++ toolchain/binutils/pass1.sh
@@ -4,6 +4,7 @@ fprint="$(mktemp)"
 
 echo $PWD/out/tools $AOSC_EC_ARCH $AOSC_EC_LIBC $AOSC_EC_TRIPLET > $fprint
 
+rm -rf $PWD/out/build/binutils-pass1
 mkdir -p $PWD/out/build/binutils-pass1
 
 utils/build_cache/cache_build binutils-pass1-$binutils_ver-$AOSC_EC_TRIPLET $fprint $PWD/out/build/binutils-pass1/destdir toolchain/binutils/pass1_build.sh
--- toolchain/binutils/pass1_build.sh
+++ toolchain/binutils/pass1_build.sh
@@ -5,11 +5,9 @@ utils/downloader/downloader toolchain/binutils/down/$binutils_ver || exit 1
 tools_prefix=$PWD/out/tools
 downloads_prefix=$PWD/out/download
 
-rm -rf out/build/binutils-pass1 &&
-mkdir -p out/build/binutils-pass1 &&
 cd out/build/binutils-pass1 &&
 
-mkdir -p destdir
+mkdir -p destdir &&
 
 tar xvf $downloads_prefix/binutils-${binutils_ver}.tar.bz2 &&
 
