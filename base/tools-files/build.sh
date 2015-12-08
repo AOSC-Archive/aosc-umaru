120387660b5b371dd016ff06cc820797c1a55c20
Icenowy Zheng <icenowy@outlook.com>
2015-12-09 00:14:35 +0800

    tools-file: strip musl libc.so

--- base/tools-files/build.sh
+++ base/tools-files/build.sh
@@ -5,6 +5,7 @@ source ./.config
 	mkdir -p out/target/{,usr/}lib/
 	cp -r out/sysroot/lib/ld-musl-*.so.1 out/target/lib/
 	cp -r out/sysroot/usr/lib/libc.so out/target/usr/lib/
+	out/tools/bin/$AOSC_EC_TRIPLET-strip --strip-debug out/target/usr/lib/libc.so
 }
 
 exit 0
