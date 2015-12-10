f0c77ae3070b75281eb6f3dc4d6f47bec33e50a6
Icenowy Zheng <icenowy@outlook.com>
2015-12-10 21:59:26 +0800

    gen_flags: fix i{4,5}86 triplet gen

--- utils/gen_flags/gen_triplet.sh
+++ utils/gen_flags/gen_triplet.sh
@@ -2,7 +2,7 @@ triplet_machine_type=aoscec
 triplet_os=linux
 
 if [ "$AOSC_EC_ARCH" = "i386" ]; then
-	triplet_cpu=$(kconfig_enum ARCH_I386_MARCH_I486 i486 ARCH_I386_MARCH_PENTIUM i586)
+	triplet_cpu=$(kconfig_enum CONFIG_ARCH_I386_MARCH_I486 i486 CONFIG_ARCH_I386_MARCH_PENTIUM i586)
 	! [ "$triplet_cpu" ] && triplet_cpu=i686
 	case $AOSC_EC_LIBC in
 		musl) triplet_abi=musl ;;
